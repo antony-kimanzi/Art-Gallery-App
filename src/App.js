9a4f1090cfd15ae56c2822a45430d2b1521a5b49
antony-kimanzi <antonykimanzi29@gmail.com>
2024-11-13 09:29:24 +0300

    created homepage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import './App.css';
 import Home from './pages/Home'
 import PersonalCollection from './pages/PersonalCollection'
-import Layout from './component/Layout';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './component/Layout';
 function App() {
   return (
-    <div className="App">
       <BrowserRouter>
        <Routes>
-        <Route path ="/" element={<Layout/>}>
+          <Route path='/' element={<Layout/>}>
           <Route index element={<Home/>}/>
           <Route path ="/personalcollection"  element={<PersonalCollection/>}/>
-        </Route>
+          </Route>
        </Routes>
       </BrowserRouter>
-    </div>
   );
 }
 
---
+++ src/component/AppFooter.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./AppFooter.css";
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <p className='cormorant-garamond-regular-italic'>Copyright GallerySphere @ 2024</p>
+    </div>
+  )
+}
