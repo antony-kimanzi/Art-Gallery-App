582622018171ee8a22fa5b4d620d90b08eda431a
Nadifo Ismail <nadifoismail80@gmail.com>
2024-11-13 16:33:33 +0300

    Save changes to Home.jsx before pulling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import ArtCards from '../component/ArtCards';
+import AddNewArt from '../component/AddNewArt';
 import {useState, useEffect} from 'react';
 import "./Home.css";
 
@@ -13,10 +14,10 @@ export default function Home() {
     .then((data) => setArtworks(data));
   },[]);
   
-  
   return (
     <div className='main montserrat-font'>
       <h1 className='home-header'>Main Gallery</h1>
+      <AddNewArt />
       <p className='home-text'>Welcome to the Main Gallery, a carefully curated collection showcasing a diverse array of artwork from talented artists around the world. Here, you'll find pieces spanning various styles, mediums, and genres, offering something for every art enthusiast. Each artwork is presented with a high-resolution preview, allowing you to explore details and gain insight into the artist’s vision and technique.</p>
       <h2 className='home-subheader'>Gallery</h2>
       <ArtCards artworks={artworks}/>
