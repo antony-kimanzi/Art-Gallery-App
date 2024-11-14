45ff9bd171a9ea1aee76b940b7b8063a3c40d884
Nadifo Ismail <nadifoismail80@gmail.com>
2024-11-14 11:21:52 +0300

    added single art

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import './App.css';
-import Home from './pages/Home'
-import PersonalCollection from './pages/PersonalCollection'
+import "./App.css";
+import Home from "./pages/Home";
+import PersonalCollection from "./pages/PersonalCollection";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from './component/Layout';
+import Layout from "./component/Layout";
+import SingleArt from "./pages/SingleArt";
 function App() {
   return (
-      <BrowserRouter>
-       <Routes>
-          <Route path='/' element={<Layout/>}>
-          <Route index element={<Home/>}/>
-          <Route path ="/personalcollection"  element={<PersonalCollection/>}/>
-          </Route>
-       </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/personalcollection" element={<PersonalCollection />} />
+          <Route path="/artwork/:id" element={<SingleArt />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
